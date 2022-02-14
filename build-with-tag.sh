36de8c2834f96d141b7dd0feb9f9a3e26736fe23
Kuvesz <kovacs.david1101@gmail.com>
2022-02-14 11:17:31 +0100

    Upgraded to Kafka 3.1.0 and changed image to alpine

--- build-with-tag.sh
+++ build-with-tag.sh
@@ -33,8 +33,10 @@ git fetch --all
 git merge
 
 scalas=$(awk '/scala_version=/' < ./Dockerfile | cut -c 19-)
-scala=$(echo $scalas | awk '{split($0,a," "); print a[1]}')
-kafka=$(awk '/kafka_version=/' < ./Dockerfile | cut -c 19-)
+# I guess the first occurence is enough as it doesn't make much sense I think to use
+# different scala and kafka versions between the images.
+scala=$(echo $scalas | awk '{split($0,a," "); print a[1]; exit}')
+kafka=$(awk '/kafka_version=/{print; exit}' < ./Dockerfile | cut -c 19-)
 major=$(awk -F. '{print $1}' < ./VERSION)
 minor=$(awk -F. '{print $2}' < ./VERSION)
 patch=$(awk -F. '{print $3}' < ./VERSION)
