f192b0137317d673ab860e556acd6479448ae148
Kuvesz <kovacs.david1101@gmail.com>
2023-02-01 22:10:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 if [ -n "$REST_ADVERTISED_HOST_NAME" ]; then
-    echo "ADDING REST_ADVERTISED_HOST_NAME"
     echo rest.advertised.host.name=$REST_ADVERTISED_HOST_NAME >> /opt/kafka/config/connect-distributed.properties
 fi
 
 if [ -n "$REST_ADVERTISED_HOST_PORT" ]; then
-    echo "ADDING REST_ADVERTISED_HOST_PORT"
     echo rest.advertised.host.port=$REST_ADVERTISED_HOST_PORT >> /opt/kafka/config/connect-distributed.properties
 fi
 
